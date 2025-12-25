dac87eaf4b10883aa3c97882a3e648e90684d473
vairakkanipv <vairakkani@gmail.com>
2025-12-25 18:14:13 +0530

    Model created and app added into installed app list and migrated

--- academy/models.py
+++ academy/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Course(models.Model):
+    course_name = models.CharField(max_length=500)
+    description = models.CharField(max_length=500)
+    duration = models.IntegerField()
+    course_image = models.ImageField(upload_to='images/')
+
+
